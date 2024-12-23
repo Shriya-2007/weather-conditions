ff2e05904015533e35cfef4f4798c47e5687b2b9
Shriya-2007 <srivastavashriya62@gmail.com>
2024-12-23 17:22:59 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,29 @@
+# import required libraries 
+import requests 
+from bs4 import BeautifulSoup 
+from win10toast import ToastNotifier 
+
+# create an object to ToastNotifier class 
+n = ToastNotifier() 
+
+# define a function 
+def getdata(url): 
+	r = requests.get(url) 
+	return r.text 
+	
+htmldata = getdata("https://www.timeanddate.com/weather/india/new-delhi/hourly") 
+
+soup = BeautifulSoup(htmldata, 'html.parser') 
+
+current_temp = soup.find_all("span", class_= "_-_-components-src-organism-CurrentConditions-CurrentConditions--tempValue--MHmYY") 
+
+chances_rain = soup.find_all("div", class_= "_-_-components-src-organism-CurrentConditions-CurrentConditions--precipValue--2aJSf") 
+
+temp = (str(current_temp)) 
+
+temp_rain = str(chances_rain) 
+
+result = "current_temp " + temp[128:-9] + " in New Delhi" + "\n" + temp_rain[131:-14] 
+n.show_toast("live Weather update",
+             result, duration = 10) 
+print(htmldata)
